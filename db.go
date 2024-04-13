a601220b3647be717096a3375a1b019e7d0e7ca4
Astr0Jay <159166944+Astr0Jay@users.noreply.github.com>
2024-04-13 01:13:02 -0500

    Update db.go

--- db.go
+++ db.go
@@ -1,43 +1,61 @@
-type user struct {
-    firstName string
-    lastName string
-    gender string
-    age int
-    sport string
-    lvl string
-    dist int
-}
+package main
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type user struct {
+	firstName string
+	lastName  string
+	gender    string
+	age       int
+	sport     string
+	lvl       string
+	dist      int
+}
 
 /*
 Dump (stdout + file) - done
 Read - done
 */
 
-func read (db string) []user {
-    var ret []user
-    d, err := os.ReadFile(db)
-    if err != nil {panic(err)}
-    rows := s.Split(string(d),"\n")
-    for _,x := range rows {
-        if len(s.Trim(x,"\n")) == 0 { return ret}
-        y := s.Split(x,",")
-        age,_ := strconv.Atoi(y[4])
-        dist,_ := strconv.Atoi(y[5])
-        ret = append(ret,user{y[0],y[1],y[3],age,y[2],y[6],dist})
-    }
-    return ret
+func read(db string) []user {
+	var ret []user
+	d, err := os.ReadFile(db)
+	if err != nil {
+		panic(err)
+	}
+	rows := strings.Split(string(d), "\n")
+	for _, x := range rows {
+		if len(strings.Trim(x, "\n")) == 0 {
+			return ret
+		}
+		y := strings.Split(x, ",")
+		age, _ := strconv.Atoi(y[4])
+		dist, _ := strconv.Atoi(y[5])
+		ret = append(ret, user{y[0], y[1], y[3], age, y[2], y[6], dist})
+	}
+	return ret
 }
 
-func stddump (u []user) {
-    for _, x := range u {fmt.Printf("%s,%s,%s,%s,%d,%d,%s\n", x.firstName,x.lastName,x.sport,x.gender,x.age,x.dist,x.lvl)}
+func stddump(u []user) {
+	for _, x := range u {
+		fmt.Printf("%s,%s,%s,%s,%d,%d,%s\n", x.firstName, x.lastName, x.sport, x.gender, x.age, x.dist, x.lvl)
+	}
 }
 
-func fdump (u []user, f string) {
-    out, err := os.OpenFile(f, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-    if err != nil { panic(err) }
-    for _, x := range u {
-        _,err:=fmt.Fprintf(out,"%s,%s,%s,%s,%d,%d,%s\n", x.firstName,x.lastName,x.sport,x.gender,x.age,x.dist,x.lvl)
-        if err != nil { panic(err) }
-    }
+func fdump(u []user, f string) {
+	out, err := os.OpenFile(f, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	for _, x := range u {
+		_, err := fmt.Fprintf(out, "%s,%s,%s,%s,%d,%d,%s\n", x.firstName, x.lastName, x.sport, x.gender, x.age, x.dist, x.lvl)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
