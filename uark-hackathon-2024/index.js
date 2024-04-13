b204625bf790e1f082ad823557a7474bba40d400
Astr0Jay <159166944+Astr0Jay@users.noreply.github.com>
2024-04-13 16:00:40 -0500

    Update index.js

--- uark-hackathon-2024/index.js
+++ uark-hackathon-2024/index.js
@@ -22,7 +22,7 @@
             <br>
             <div class="button-container">
                 <button class="dislike-button" style="background-color: #e74c3c; color: white; padding: 0.5rem 1rem; border-radius: 0.375rem;">Disike</button>
-                <button class="like-button" style="background-color: #34D399; color: white; padding: 0.5rem 1rem; border-radius: 0.375rem;">Like</button>
+                <button class="like-button" style="background-color: #34D399; color: white; padding: 0.5rem 1rem; border-radius: 0.375rem;">Play Ball!</button>
             </div>
         `;
         cardContainer.appendChild(card);
